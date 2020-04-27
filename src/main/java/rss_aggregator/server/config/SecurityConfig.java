fb3f04c87f96a014d95d7f5922e15b7f3156d454
Artous <thibautartis@protonmail.ch>
2020-04-27 23:10:24 +0200

    authorize non authed req to passwordlost

--- src/main/java/rss_aggregator/server/config/SecurityConfig.java
+++ src/main/java/rss_aggregator/server/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().and().csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/login", "/signup", "/signupConfirm", "/resendSignupToken").permitAll()
-                    .antMatchers(HttpMethod.POST, "/login").permitAll()
+                    .antMatchers(HttpMethod.POST, "/login", "/passwordLost", "/changePasswordLost").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
